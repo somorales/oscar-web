c7902a8f198f80bf237d1f2f42eb3eb4014c068f
Sofia Morales <sofimoralesroa92@gmail.com>
2024-12-10 11:47:07 -0300

    primera pagina home page

--- src/componentes/NavBar.jsx
+++ src/componentes/NavBar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import oscarLogo from "../assets/imagenes/oscar-logo.png";
+import oscarLogoBlanco from  "../assets/imagenes/oscar-logo-blanco.png"
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 
@@ -7,7 +8,7 @@ export default function NavBar() {
   const navigate = useNavigate();
 
   return (
-    <header className="relative z-50 w-full flex-none text-sm font-semibold leading-6 text-slate-900 bg-ao">
+    <header className="relative z-50 w-full flex-none text-sm font-semibold leading-6 text-slate-900 bg-[#392F5A]">
       <nav
         aria-label="Global"
         className="mx-auto max-w-container px-4 sm:px-6 lg:px-36"
@@ -17,21 +18,21 @@ export default function NavBar() {
             <div className="h-6 w-auto">
               <Link to="/">
                 <span className="sr-only">Oscar</span>
-                <img alt="" src={oscarLogo} className="h-8 w-auto" />
+                <img alt="" src={oscarLogoBlanco} className="h-8 w-auto" />
               </Link>
             </div>
           </div>
 
           <div className="hidden lg:flex lg:justify-end lg:w-full">
             <>
-              <Link to="/about" className="hover:text-[#c07c53]">
+              <Link to="/about" className="ml-8 text-white hover:text-lg ">
                 About me
               </Link>
-              <Link className="ml-8 hover:text-[#c07c53]  pl-[1.5rem]" to="/work">
+              <Link className="ml-8 text-white hover:text-lg pl-[1.5rem]" to="/work">
                 Work
               </Link>
 
-              <Link className="ml-8 hover:text-[#c07c53] pl-[1.5rem]" to="/contact">
+              <Link className="ml-8 text-white hover:text-lg pl-[1.5rem]" to="/contact">
                 Contact
               </Link>
             </>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React from "react";
+import cat from "../assets/imagenes/cat.svg";
 
 export default function HomePage() {
   return (
-    <div>
-      Homepage
+    <div className="bg-[#392F5A]">
+      <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+        <div className="hidden sm:mb-8 sm:flex sm:justify-center"></div>
+        <div className="text-center">
+          <div className="flex">
+          <img src={cat} alt="" />
+          <h1 className="text-balance text-5xl font-semibold tracking-tight text-white sm:text-7xl pl-[1.5rem]">
+            Hey, Oscar here!
+          </h1>
+          </div>
+          <h2 className="mt-8 text-pretty font-medium text-white sm:text-4xl">
+            UX/UI Designer based in Barcelona
+          </h2>
+          <p className="mt-8 text-pretty text-lg font-medium text-white sm:text-xl/8">
+            Passionate about creating digital experiences with user-focused
+            design principles. I also have a background in digital marketing,
+            design, and photography. Thanks for visiting!
+          </p>
+          <div className="mt-10 flex items-center justify-center gap-x-6">
+            <a
+              href="#"
+              className="rounded-3xl bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              Download my CV
+            </a>
+          </div>
+        </div>
+      </div>
+      <div
+        aria-hidden="true"
+        className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
+      ></div>
     </div>
-  )
+  );
 }
