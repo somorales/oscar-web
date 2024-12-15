0a23eafd81acc7538c7e853df638cf8dc4c8efe2
Sofia Morales <sofimoralesroa92@gmail.com>
2024-12-15 16:12:58 -0300

    katia Dental Project

--- src/componentes/KatiaDentalProject.jsx
+++ src/componentes/KatiaDentalProject.jsx
@@ -13,15 +13,13 @@ export default function KatiaDEntalProyect() {
       </div>
 
       <div className="max-w-7xl mx-auto">
-        
         <div className="mb-6">
-        <h2 className="text-l font-mediun mb-3">
-        This was a project I worked on when I was a designer at “Cronuts Digital”
+          <h2 className="text-l font-mediun mb-3">
+            This was a project I worked on when I was a designer at “Cronuts
+            Digital”
           </h2>
 
-          <h2 className="text-lg font-semibold mb-3">
-           Frist Steps:
-          </h2>
+          <h2 className="text-lg font-semibold mb-3">Frist Steps:</h2>
           <ul className="space-y-2">
             <li className="flex items-start space-x-2">
               <span className="text-blue-500 font-bold">•</span>
@@ -42,6 +40,73 @@ export default function KatiaDEntalProyect() {
                 Analyze competitor landing pages to identify what works well and
                 what doesn't
               </span>
+            </li>
+          </ul>
+        </div>
+
+        <div className="mb-6">
+          <h2 className="text-lg font-semibold mb-3">Design:</h2>
+          <ul className="space-y-2">
+            <li className="flex items-start space-x-2">
+              <span className="text-blue-500 font-bold">•</span>
+              <span>
+                Create a basic layout to outline the structure, including where
+                images, text, and CTAs will be placed.
+              </span>
+            </li>
+            <li className="flex items-start space-x-2">
+              <span className="text-blue-500 font-bold">•</span>
+              <span>
+                Ensure the headline is compelling and communicates the value
+                proposition immediately.
+              </span>
+            </li>
+            <li className="flex items-start space-x-2">
+              <span className="text-blue-500 font-bold">•</span>
+              <span>
+                Design minimalistic visuals that leads the user’s eye toward the
+                goal.
+              </span>
+            </li>
+            <li className="flex items-start space-x-2">
+              <span className="text-blue-500 font-bold">•</span>
+              <span>
+                Use the color palette provided by the clients existing branding.
+              </span>
+            </li>
+            <li className="flex items-start space-x-2">
+              <span className="text-blue-500 font-bold">•</span>
+              <span>Design the site in Wordpress and Elementor.</span>
+            </li>
+          </ul>
+        </div>
+
+        <div className="mb-6">
+          <h2 className="text-lg font-semibold mb-3">Learnings:</h2>
+          <ul className="space-y-2">
+            <li className="flex items-start space-x-2">
+              <span>
+                The first lesson I learned was the importance of fully
+                understanding the client’s business objectives and target
+                audience. It was definitely a highly rewarding and insightful
+                experience. The project allowed me to sharpen both my design and
+                communication skills, leading to a successful outcome that met
+                the client’s expectations.
+              </span>
+            </li>
+          </ul>
+        </div>
+
+        <div className="mb-6">
+          <h2 className="text-lg font-semibold mb-3"> Results:</h2>
+          <ul className="space-y-2">
+            <li className="flex items-start space-x-2">
+              <span className="text-blue-500 font-bold">•</span>
+              <span>Increased “leads” for Katia Dental</span>
+            </li>
+            <li className="flex items-start space-x-2">
+              <span className="text-blue-500 font-bold">•</span>
+              <span>Update to a modern and user centered design web page</span>
             </li>
           </ul>
         </div>
