fc04995d17344dcfb2af78947bb2af4b0393ee77
Jorge Ramírez <jorgeramirez1990@gmail.com>
2024-12-15 22:58:54 -0300

    ajustes katia dental

--- src/pages/KatiaDental.jsx
+++ src/pages/KatiaDental.jsx
@@ -1,8 +1,8 @@
-import React from 'react'
-import { useContext, useEffect } from 'react';
+import React from "react";
+import { useContext, useEffect } from "react";
 import desktop from "../assets/imagenes/desktop_Katia2.png";
-import KatiaDentalProject from '../componentes/KatiaDentalProject';
-import { NavContext } from '../context/nav.context';
+import KatiaDentalProject from "../componentes/KatiaDentalProject";
+import { NavContext } from "../context/nav.context";
 import oscarLogo from "../assets/imagenes/oscar-logo.png";
 
 export default function KatiaDental() {
@@ -10,38 +10,40 @@ export default function KatiaDental() {
 
   useEffect(() => {
     setBackgroundColor("bg-[#A3E6E6]");
-    setTextColor("text-black")
-    setLogo(oscarLogo)
+    setTextColor("text-black");
+    setLogo(oscarLogo);
   });
 
   return (
     <div>
-      <div className="min-h-screen bg-[#A3E6E6] flex items-center justify-between p-8 lg:p-16">
+      <div className="mx-auto max-w-container px-6 sm:px-8 lg:px-48 bg-[#A3E6E6] grid grid-cols-5 pb-8 lg:pb-16 items-end relative">
         {/* Left side content */}
-        <div className="space-y-4 flex-1">
+        <div className="col-span-2">
           <h1 className="text-4xl lg:text-6xl font-bold text-[#0B9B9B]">
             Katia Dental
           </h1>
           <p className="text-xl lg:text-2xl text-[#0B9B9B] max-w-md">
-          Landing Page Design for a real client in Barcelona
+            Landing Page Design for a real client in Barcelona
           </p>
         </div>
 
         {/* Right side placeholder for phone image */}
-        <div className="w-64 lg:w-72">
-          <img src={desktop} alt="phone" className="w-full h-full object-cover" />
+        <div className="min-h-[500px] col-span-3">
+          <img
+            src={desktop}
+            alt="phone"
+            className="h-[450px] object-cover absolute bottom-1 mb-10"
+          />
         </div>
       </div>
 
-      < KatiaDentalProject />
-      
+      <KatiaDentalProject />
+
       <div className="my-10 flex flex-col items-center justify-center gap-2">
-        {/* <p className="text-red-500 text-sm">Link a behance</p> */}
         <button className="bg-white border-2 border-black rounded-3xl hover:bg-gray-200 px-3.5 py-2.5 shadow-sm w-60 font-bold">
           More info
         </button>
       </div>
-
     </div>
-  )
+  );
 }
