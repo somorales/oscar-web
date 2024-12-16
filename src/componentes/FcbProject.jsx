6433b874df58210472cb5a4c4eab2c7e5c7d26e4
Sofia Morales <sofimoralesroa92@gmail.com>
2024-12-15 23:02:04 -0300

    fcb detalles

--- src/componentes/FcbProject.jsx
+++ src/componentes/FcbProject.jsx
@@ -4,60 +4,59 @@ import screens from "../assets/imagenes/screns_barca.png";
 export default function FcbProject() {
   return (
     <div>
-      <div className="max-w-7xl mx-auto">
-        <div className="mb-6">
-          <div className="p-4">
-            <h2 className="text-l font-semibold mb-3">
-              This was a project I worked on when I was a designer at “Cronuts
-              Digital”
-            </h2>
-          </div>
-
-          <h2 className="text-lg font-semibold mb-3">
-            {" "}
-            Meeting the client needs:
+      <div className="mx-auto max-w-container px-6 sm:px-8 lg:px-48">
+        <div className="pt-20">
+          <h2 className="text-2xl font-bold mb-4">
+            This was a project I worked on when I was a designer at “Cronuts
+            Digital”
           </h2>
-          <ul className="space-y-2">
-            <li className="flex items-start space-x-2">
-              <span className="text-blue-500 font-bold">•</span>
-              <span>
-                The client wanted to develop the mobile version of their
-                existing website. They asked us to develop the low-fidelity
-                wireframes based on the website they had.
-              </span>
-            </li>
-            <li className="flex items-start space-x-2">
-              <span className="text-blue-500 font-bold">•</span>
-              <span>
-                Although wireframes are a very basic form of design, they also
-                wanted to keep the UX Writing used in the web for they to
-                evaluate if it needed to be changed.
-              </span>
-            </li>
-            <li className="flex items-start space-x-2">
-              <span className="text-blue-500 font-bold">•</span>
-              <span>At the end, they also asked for high-fi wireframes.</span>
-            </li>
-          </ul>
+          <div className="mb-6">
+            <h2 className="text-lg font-semibold mb-3">
+              {" "}
+              Meeting the client needs:
+            </h2>
+            <ul className="space-y-2">
+              <li className="flex items-start space-x-2">
+                <span className="text-black font-bold">•</span>
+                <span>
+                  The client wanted to develop the mobile version of their
+                  existing website. They asked us to develop the low-fidelity
+                  wireframes based on the website they had.
+                </span>
+              </li>
+              <li className="flex items-start space-x-2">
+                <span className="text-black font-bold">•</span>
+                <span>
+                  Although wireframes are a very basic form of design, they also
+                  wanted to keep the UX Writing used in the web for they to
+                  evaluate if it needed to be changed.
+                </span>
+              </li>
+              <li className="flex items-start space-x-2">
+                <span className="text-black font-bold">•</span>
+                <span>At the end, they also asked for high-fi wireframes.</span>
+              </li>
+            </ul>
+          </div>
         </div>
 
         <div className="mb-6">
           <h2 className="text-lg font-semibold mb-3"> Process: </h2>
           <ul className="space-y-2">
             <li className="flex items-start space-x-2">
-              <span className="text-blue-500 font-bold">•</span>
+              <span className="text-black font-bold">•</span>
               <span>Analyzed the existing web version.</span>
             </li>
             <li className="flex items-start space-x-2">
-              <span className="text-blue-500 font-bold">•</span>
+              <span className="text-black font-bold">•</span>
               <span>Simplified sections and build basic forms.</span>
             </li>
             <li className="flex items-start space-x-2">
-              <span className="text-blue-500 font-bold">•</span>
+              <span className="text-black font-bold">•</span>
               <span>Created components.</span>
             </li>
             <li className="flex items-start space-x-2">
-              <span className="text-blue-500 font-bold">•</span>
+              <span className="text-black font-bold">•</span>
               <span> Client feedback and iterate.</span>
             </li>
           </ul>
@@ -67,20 +66,20 @@ export default function FcbProject() {
           <h2 className="text-lg font-semibold mb-3"> Learnings: </h2>
           <ul className="space-y-2">
             <li className="flex items-start space-x-2">
-              <span className="text-blue-500 font-bold">•</span>
+              <span className="text-black font-bold">•</span>
               <span>
                 Realize how essential it is to keep things simple and flexible.
               </span>
             </li>
             <li className="flex items-start space-x-2">
-              <span className="text-blue-500 font-bold">•</span>
+              <span className="text-black font-bold">•</span>
               <span>
                 Managing visual consistency, because we have to do it with an
                 existing material.
               </span>
             </li>
             <li className="flex items-start space-x-2">
-              <span className="text-blue-500 font-bold">•</span>
+              <span className="text-black font-bold">•</span>
               <span>
                 When we reached the high-fi wireframe design, I needed to work
                 directly with a Design System handed-off by the client, and that
@@ -88,7 +87,7 @@ export default function FcbProject() {
               </span>
             </li>
             <li className="flex items-start space-x-2">
-              <span className="text-blue-500 font-bold">•</span>
+              <span className="text-black font-bold">•</span>
               <span>
                 {" "}
                 Loved the experience to working with a international team.
@@ -101,7 +100,7 @@ export default function FcbProject() {
           <h2 className="text-lg font-semibold mb-3"> Results: </h2>
           <ul className="space-y-2">
             <li className="flex items-start space-x-2">
-              <span className="text-blue-500 font-bold">•</span>
+              <span className="text-black font-bold">•</span>
               <span>
                 They managed to match their website with the mobile version and
                 now is available.
--- src/pages/FcbBarcelona.jsx
+++ src/pages/FcbBarcelona.jsx
@@ -1,13 +1,26 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { useContext } from "react";
 import phone from "../assets/imagenes/screens_barca.png";
 import FcbProject from '../componentes/FcbProject'
+import { NavContext } from "../context/nav.context";
+import oscarLogo from "../assets/imagenes/oscar-logo.png";
 
 export default function FcbBarcelona() {
+  const { setBackgroundColor, setTextColor, setLogo } = useContext(NavContext);
+
+  useEffect(() => {
+    setBackgroundColor("bg-[#FBB5C3]");
+    setTextColor("text-black")
+    setLogo(oscarLogo)
+  });
   return (
     <div>
-      <div className="min-h-screen bg-[#FBB5C3] flex items-center p-8 lg:p-16">
+       <div className="bg-[#FBB5C3]">
+
+       
+      <div className="mx-auto max-w-container px-6 sm:px-8 lg:px-48 min-h-screen grid grid-cols-2 items-end">
         {/* Left side content */}
-        <div>
+        <div className="pb-[11rem]">
           <h1 className="lg:text-7xl font-bold text-[#9E0925] pb-4">FCB Barça Academy</h1>
           <p className="lg:text-3xl text-[#9E0925] max-w-md">
           Wireframe design project for a real client based in th US
@@ -15,18 +28,19 @@ export default function FcbBarcelona() {
         </div>
 
         {/* Right side placeholder for phone image */}
-        <div className="flex flex-1 justify-end">
+        <div className="flex justify-center pb-32">
           <img
             src={phone}
             alt="phone"
-            className="h-[600px] w-64 lg:w-72 object-cover"
+            className="h-[600px] object-cover"
           />
+        </div>
         </div>
       </div>
       <FcbProject />
       <div className="my-10 flex flex-col items-center justify-center gap-2">
         {/* <p className="text-red-500 text-sm">Link a behance</p> */}
-        <button className="bg-white border-2 border-black rounded-3xl hover:bg-gray-200 px-3.5 py-2.5 shadow-sm w-60 font-bold">
+        <button className="bg-white border-2 border-black rounded-3xl hover:bg-[#52B6B9] px-3.5 py-2.5 shadow-sm w-60 font-bold">
           Web
         </button>
       </div>
